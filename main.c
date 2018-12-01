2dfff1188967939fb648ea7003a353e2719fe04b
joao96 <you@example.com>
2018-12-01 19:46:57 -0200

    add functions to main module

--- main.c
+++ main.c
@@ -9,38 +9,61 @@
 #include <netdb.h>
 #include "ServidorSocket.c"
 
-int actual_socket = 0;
-int new_socket = 0;
 
 //porta
 #define porta 8228
 
-// tamanho do buffer
-int BUFFER_SIZE = 4096;
 
 int main(){
 
+    // sockets do servidor e cliente
     int actual_socket, new_socket;
+    // tamanho do buffer
+    int BUFFER_SIZE = 4096;
     int int_pointer = 1;
-    struct sockaddr_in servidor;
-    struct sockaddr_in cliente;
+    int message_len;
+    struct sockaddr_in servidor, cliente;
     char *buf = malloc(BUFFER_SIZE);
     unsigned int addr_len;
-    socket_create(&actual_socket, &servidor);
-    config_socket(actual_socket, &int_pointer);
+
+    // cria o socket do servidor
+    actual_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(actual_socket < 0){
+        perror("Nao foi possivel criar o socket do servidor.\n");
+    }
+    printf("Socket criado com sucesso, %d.\n", actual_socket);
+    servidor.sin_family = AF_INET;
+    servidor.sin_addr.s_addr = INADDR_ANY;
+    // conecta-se a porta 8228
+    servidor.sin_port = htons(porta);
 
     addr_len = sizeof(struct sockaddr_in);
 
-    bind_socket(actual_socket, servidor, addr_len);
+    // conecta o servidor ao socket criado
+    if(bind(actual_socket, (struct sockaddr*)&servidor, sizeof(servidor)) < 0) {
+        perror("Bindou errado.\n");
+    }
 
     do {
-        listen_socket(actual_socket);
+      // ouve a porta
+      if(listen(actual_socket, 1) == -1) {
+        perror("Escutou errado.\n");
+      }
+      printf("Porta: %d\n", porta);
 
-        accept_socket(cliente, actual_socket, &new_socket);
-
-        recv_socket(buf, new_socket);
-
-        close(new_socket);
+      int cliente_lenght = sizeof(cliente);
+      // aceita o cliente que deseja-se conectar ao servidor e reserva um socket para ele
+      if ((new_socket = accept(actual_socket,(struct sockaddr *) &cliente, &cliente_lenght)) == -1) {
+          perror("Erro ao aceitar o cliente.\n");
+      }
+      printf("Aceitou.\n");
+      // recebe o que o cliente quer transmitir
+      if((message_len = recv(new_socket, buf, BUFFER_LENGTH, 0)) > 0) {
+          buf[message_len - 1] = '\0';
+          printf("Cliente fala: %s.\n", buf);
+      }
+      // fecha o socket do cliente
+      close(new_socket);
 
     } while(1);
 

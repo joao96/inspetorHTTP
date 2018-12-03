e9b2fcb15d89a621eeb1de288e8144f958fc2c80
joao96 <you@example.com>
2018-12-03 03:18:52 -0200

    directory implementation on dump function

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int BUFFER_SIZE = 4096; // tamanho do buffer
 void parsing(char*, char*, char*);
 int get_host_by_name(char*, char*);
 FILE *spider(char*);
-void dump(FILE*, char*, char*);
+void dump(char*);
 FILE *create_html_txt(char*);
 
 int main(){
@@ -93,7 +93,7 @@ int main(){
     }
 
     html_tree = spider(new_host);
-    //dump(html_tree, new_host, new_http);
+    dump(new_host);
 
     fclose(html_tree);
     close(new_socket); // fecha o socket do cliente
@@ -162,12 +162,12 @@ FILE * spider(char *host) {
      html_tree = fopen("html_tree.txt", "a");
 
      if(html_file == NULL){
-         printf("Erro ao abrir o arquivo.\n");
+         printf("Erro ao abrir o arquivo. 1\n");
          exit(1);
      }
 
     if(html_tree == NULL){
-        printf("Erro ao abrir o arquivo.\n");
+        printf("Erro ao abrir o arquivo. 2\n");
         exit(2);
     }
     while(getline(&href, &href_size, html_file) != -1) {
@@ -191,11 +191,13 @@ FILE * spider(char *host) {
     return html_tree;
 }
 
-void dump(FILE *html_tree, char *host, char *http) {
+void dump(char *host) {
+    FILE *html_tree;
     struct hostent *hp;
     struct sockaddr_in cliente;
     int on = 1, sock;
     long int i, j = 0, k = 0;
+    char l;
     char *buf = malloc(BUFFER_SIZE);
     char dir[150];
     char reverse_dir[150];
@@ -209,11 +211,12 @@ void dump(FILE *html_tree, char *host, char *http) {
     html_tree = fopen("html_tree.txt", "r");
 
     if(html_tree == NULL){
-        printf("Erro ao abrir o arquivo\n");
+        printf("Erro ao abrir o arquivo. 3\n");
         exit(3);
     }
 
     while (fgets(href, sizeof(href), html_tree)) {
+        j = 0, k = 0, i = 0;
         if ((hp = gethostbyname(host)) == NULL){
             herror("gethostbyname");
         }
@@ -246,11 +249,24 @@ void dump(FILE *html_tree, char *host, char *http) {
             k = 0;
             mkdir(dir, S_IRUSR | S_IWUSR | S_IXUSR);
         }
+        bzero(reverse_dir, 150);
+        if((needle = strstr(href, "\r\n")) != NULL){
+            i = needle - href - 1;
+            j = 0;
+            while((c = href[i]) != '/')
+                i--;
+            while(((c = href[i+1]) != '\r')){
+
+                reverse_dir[j] = c;
+                i++;
+                j++;
+            }
+        }
         needle = strstr(href, host);
         i = needle - href + 0;
         k = 0;
         while(href[i] != '/')
-            i++;
+        i++;
         while(href[i] != '\0'){
             href[k] = href[i];
             k++;
@@ -265,17 +281,24 @@ void dump(FILE *html_tree, char *host, char *http) {
         printf("%s\n", request);
         write(sock, request, strlen(request));
         memset(buf, 0x0, BUFFER_SIZE); //limpa o buffer antigo
-        strcat(dir, "/");
-        strcat(dir, "a");
+        printf("%s\n", reverse_dir);
+        if(strlen(dir) != 0){
+            strcat(dir, "/");
+            strcat(dir, reverse_dir);
+            printf("1: %s\n", dir);
+        }else{
+            strcpy(dir, reverse_dir);
+            printf("2: %s\n", dir);
+        }
         FILE *file = fopen(dir, "w");
         if(file != NULL){
             while(read(sock, buf, BUFFER_SIZE-1) != 0){
-                fprintf(stderr,"%s", buf);
                 fputs(buf, file);
+                bzero(buf, BUFFER_SIZE);
             }
         }
         else{
-            printf("Erro ao abrir o arquivo\n");
+            printf("Erro ao abrir o arquivo 5.\n");
             exit(5);
         }
         close(sock);

fcb6ebbddcf03b4f956af50d57b3e25af5b5efd4
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2018-12-02 23:12:15 -0200

    Spider and dump incomplete.

--- main.c
+++ main.c
@@ -8,20 +8,24 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <netdb.h>
+#include <sys/stat.h>
 
 
-//porta
-#define porta 8228
+
+#define porta 8228 //porta
+
 void parsing(char*, char*, char*);
-int get_host_by_name(char*, char*, int);
-void spider(char *);
+int get_host_by_name(char*, char*);
+FILE *spider(char*, FILE*, char*);
+void dump(FILE*, char*, char*, int);
+FILE *create_html_txt(char*);
+
 int main(){
 
-    // sockets do servidor e cliente
-    int actual_socket, new_socket;
-    // tamanho do buffer
-    int BUFFER_SIZE = 200000000;
-    int message_len;
+    FILE *html_file = NULL, *html_tree = NULL;
+    int actual_socket, new_socket; // sockets do servidor e cliente
+    int BUFFER_SIZE = 200000000; // tamanho do buffer
+    long int message_len;
     struct sockaddr_in servidor, cliente;
     char *buf = malloc(BUFFER_SIZE);
     char new_http[150] = "", new_host[150] = "";
@@ -34,10 +38,10 @@ int main(){
         perror("Nao foi possivel criar o socket do servidor.\n");
     }
     printf("Socket criado com sucesso, %d.\n", actual_socket);
+
     servidor.sin_family = AF_INET;
     servidor.sin_addr.s_addr = INADDR_ANY;
-    // conecta-se a porta 8228
-    servidor.sin_port = htons(porta);
+    servidor.sin_port = htons(porta); // conecta-se a porta 8228
 
     addr_len = sizeof(struct sockaddr_in);
 
@@ -54,48 +58,39 @@ int main(){
         printf("Porta: %d\n", porta);
 
         unsigned int cliente_lenght = sizeof(cliente);
+
         // aceita o cliente que deseja-se conectar ao servidor e reserva um socket para ele
         if ((new_socket = accept(actual_socket,(struct sockaddr *) &cliente, &cliente_lenght)) == -1) {
             perror("Erro ao aceitar o cliente.\n");
         }
         printf("Aceitou.\n");
 
-        // recebe o request
-        if((message_len = recv(new_socket, buf, BUFFER_SIZE, 0)) > 0) {
+
+        if((message_len = recv(new_socket, buf, BUFFER_SIZE, 0)) > 0) {  // recebe o request
             buf[message_len - 1] = '\0';
-            printf("Request: %s.\n", buf);
+            //printf("Request: %s.\n", buf);
         }
 
-        // cria novo url para mandar ao cliente
-        memset(new_http, 0x0, 150);
+
+        memset(new_http, 0x0, 150); // cria novo url para mandar ao cliente
         parsing(buf, new_http, new_host);
 
-        int sock=get_host_by_name(new_http, new_host, actual_socket);
+        // faz novo GET para o cliente
+        int sock = get_host_by_name(new_http, new_host);
 
-        /*
-         // envia novo url
-         if(send(actual_socket, new_http, strlen(new_http), 0)){
-         printf("Novo pedido por: %s", new_http);
-         }
-         */
-        //limpa o buffer antigo
-        memset(buf, 0x0, BUFFER_SIZE);
 
-        // espera resposta do cliente
-        /*if((message_len = recv(sock, buf, BUFFER_SIZE, 0)) > 0) {
-            buf[message_len - 1] = '\0';
-            printf("Resposta: %s.\n", buf);
-        }*/
+        memset(buf, 0x0, BUFFER_SIZE); //limpa o buffer antigo
 
-        while(read(sock,buf,BUFFER_SIZE-1)!=0){
-            fprintf(stderr,"%s",buf);
-            spider(buf);
+
+        while(read(sock, buf, BUFFER_SIZE-1)!=0){
+            html_file = create_html_txt(buf);
             bzero(buf, BUFFER_SIZE);
         }
 
+        html_tree = spider(buf, html_file, new_host);
+        dump(html_tree, new_host, new_http, sock);
 
-        // fecha o socket do cliente
-        close(new_socket);
+        close(new_socket); // fecha o socket do cliente
 
    // } while(1);
 }
@@ -105,17 +100,16 @@ void parsing(char* buf, char * new_http, char * new_host){
     char *http = strstr(buf, "HTTP/1.1");
     char *host = strstr(buf, "Host:");
     char *user_agent = strstr(buf, "User-Agent");
-    int i, j=0;
-    int start_http = get - buf + 4;
-    int end_http = http - buf - 2;
-    int start_host = host - buf + 6;
-    int end_host = user_agent - buf - 2;
+    long int i, j=0;
+    long int start_http = get - buf + 4;
+    long int end_http = http - buf - 2;
+    long int start_host = host - buf + 6;
+    long int end_host = user_agent - buf - 2;
     for(i=start_http;i<end_http;i++){
         new_http[j] = buf[i];
         j++;
     }
     strcat(new_http, "/index.html");
-    printf("concat: %s", new_http);
     j = 0;
     for(i=start_host;i<end_host;i++){
         new_host[j] = buf[i];
@@ -123,7 +117,7 @@ void parsing(char* buf, char * new_http, char * new_host){
     }
 }
 
-int get_host_by_name(char *new_http, char *new_host, int actual_socket){
+int get_host_by_name(char *new_http, char *new_host){
     struct hostent *hp;
     struct sockaddr_in cliente;
     int on = 1, sock;
@@ -148,31 +142,128 @@ int get_host_by_name(char *new_http, char *new_host, int actual_socket){
     return sock;
 }
 
-void spider(char *html){
-    FILE *html_tree, *html_page;
-    char needle[1] = "";
+FILE * spider(char *html, FILE *html_file, char *host){
+     FILE *html_tree;
+     char href[256], buf[2000000], c;
+     char *needle;
+     long int i, j=0;
 
-    html_tree = fopen("html_tree.txt", "w");
-    html_page = fopen("html_page.txt", "w");
+     html_file = fopen("html_file.txt", "r");
+     html_tree = fopen("html_tree.txt", "a");
 
-    if (html_tree == NULL) {
+     if(html_file == NULL){
+         printf("Erro ao abrir o arquivo\n");
+         exit(0);
+     }
+
+    if(html_tree == NULL){
         printf("Erro ao abrir o arquivo\n");
         exit(0);
     }
-    if (html_page == NULL) {
-        printf("Erro ao abrir o arquivo\n");
-        exit(0);
-    }
 
-    fputs(html, html_page);
+     while (fgets(href, sizeof(href), html_file)) {
 
-    while(!feof(html_page)){
-        char *href = strstr(html, "<a href=");
-        href = href + 7;
-        while(*href!= '>'){
-            fprintf(html_tree, "%s", href);
+         if((needle = strstr(href, "href=")) != NULL){
+             i = needle - href + 6;
+             while((c = href[i])!= '"'){
+                 buf[j] = c;
+                 i++;
+                 j++;
+             }
+             strcat(buf, "\r\n");
+             if(strstr(buf, host) != NULL){
+                 fputs(buf, html_tree);
+             }
+             bzero(buf, 2000000);
+             j = 0;
         }
+     }
+    fclose(html_file);
+    fclose(html_tree);
+    return html_tree;
+}
 
+void dump(FILE *html_tree, char *host, char *http, int sock){
+
+    long int i, j = 0, k = 0;
+    char *buf = malloc(200000000);
+    char dir[150];
+    char reverse_dir[150];
+    char href[256], c;
+    char *needle;
+    bzero(dir, 150);
+    bzero(reverse_dir, 150);
+    dir[0] = '\0';
+    reverse_dir[0] = '\0';
+
+    html_tree = fopen("html_tree.txt", "r");
+
+    if(html_tree == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        exit(0);
     }
 
+    while (fgets(href, sizeof(href), html_tree)) {
+        memset(dir, '\0', 150);
+        memset(reverse_dir, '\0', 150);
+        if((needle = strstr(href, "\r\n")) != NULL){
+            i = needle - href - 1;
+            while((c = href[i]) != '/')
+                i--;
+            while(((c = href[i-1]) != '/')){
+                reverse_dir[j] = c;
+                j++;
+                i--;
+            }
+            j = strlen(reverse_dir);
+            while(j > 0){
+                dir[k] = reverse_dir[j-1];
+                j--;
+                k++;
+            }
+            k = 0;
+            mkdir(dir, S_IRUSR | S_IWUSR | S_IXUSR);
+        }
+        needle = strstr(href, host);
+        i = needle - href + 0;
+        k = 0;
+        while(href[i] != '/')
+            i++;
+        while(href[i] != '\0'){
+            href[k] = href[i];
+            k++;
+            i++;
+        }
+        href[k] = '\0';
+        char request[1000] = "GET ";
+        strcat(request, href);
+        strcat(request, "HTTP/1.1\r\nHost: ");
+        strcat(request, host);
+        strcat(request, "\r\n\r\n");
+        printf("%s\n", request);
+        write(sock, request, strlen(request));
+        memset(buf, 0x0, 200000000); //limpa o buffer antigo
+        while(read(sock, buf, 200000000-1) != 0){
+            printf("entrouuu");
+            fprintf(stderr,"%s",buf);
+            strcat(dir, "/");
+            strcat(dir,buf);
+            FILE *file = fopen(dir, "w");
+            fputs(buf, file);
+        }
+    }
+
+}
+
+FILE * create_html_txt(char *html){
+
+    FILE * html_file = fopen("html_file.txt", "a");
+
+    if(html_file == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        exit(0);
+    }
+
+    fputs(html, html_file);
+    return html_file;
 }
